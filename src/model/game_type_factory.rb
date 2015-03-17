f41fbbe74381e93e594b30e638a52c9c0debc61c
Ryan <thornhil@e5-05-03.ece.ualberta.ca>
2015-03-17 15:24:45 -0600

    Added factory contracts

--- src/model/game_type_factory.rb
+++ src/model/game_type_factory.rb
@@ -1,6 +1,23 @@
+require_relative '../util/common_contracts.rb'
+require_relative 'game_config.rb'
+
 class GameTypeFactory
 
-  def initialize
+  include GameTypeFactoryContracts
 
+  def initialize(gamconfig)
+  	verify_type(type)
   end
+
+  def buildGameType()
+  end
+
+  def setPlayers(number)
+  	CommonContracts.integers(number)
+  end
+
+  def setAIdifficulty(difficulty)
+  	CommonContracts.verify_difficulty(difficulty)
+  end
+
 end
