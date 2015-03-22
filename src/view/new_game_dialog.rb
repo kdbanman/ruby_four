00205a72d23beb8d41b000a7eb80d6799aa33619
ryant26 <ryant26@gmail.com>
2015-03-21 18:39:41 -0600

    Players, dyanmic actions working

--- src/view/new_game_dialog.rb
+++ src/view/new_game_dialog.rb
@@ -2,11 +2,20 @@ require 'gtk2'
 
 class NewGameDialog
 
+  TOOT_RADIO_BUTTON = 'toot_radio_button'
+  CONNECT4_RADIO_BUTTON = 'connect4_radio_button'
+
+  PLAYERS0_RADIO_BUTTON = 'players0_radio_button'
+  PLAYERS1_RADIO_BUTTON = 'players1_radio_button'
+  PLAYERS2_RADIO_BUTTON = 'players2_radio_button'
+
+  MAIN_WINDOW = 'main_window'
+
   def initialize
     Gtk.init
     @builder = Gtk::Builder.new
     @builder.add_from_file('../resources/new_game_dialogue.glade')
-    @mainWindow = @builder.get_object('main_window')
+    @mainWindow = @builder.get_object(MAIN_WINDOW)
     set_up_game_type
     set_up_players
     set_up_difficulty
@@ -28,8 +37,8 @@ class NewGameDialog
 
   private
   def set_up_game_type
-    tootButton = @builder.get_object('toot_radio_button')
-    c4Button = @builder.get_object('connect4_radio_button')
+    tootButton = @builder.get_object(TOOT_RADIO_BUTTON)
+    c4Button = @builder.get_object(CONNECT4_RADIO_BUTTON)
 
     c4Button.group = tootButton
 
@@ -38,23 +47,52 @@ class NewGameDialog
       groupItem.signal_connect('toggled') do |button|
         if button.active?
           #todo could be extracted into class if have time
-          @gametype = :toot if button.name == 'toot_radio_button'
-          @gametype = :connect4 if button.name == 'connect4_radio_button'
+          @gametype = :toot if button == tootButton
+          @gametype = :connect4 if button == c4Button
         end
       end
     end
   end
 
   def set_up_players
-    zeroButton = @builder.get_object('players0_radio_button')
-    oneButton = @builder.get_object('players1_radio_button')
-    twoButton = @builder.get_object('players2_radio_button')
+    zeroButton = @builder.get_object(PLAYERS0_RADIO_BUTTON)
+    oneButton = @builder.get_object(PLAYERS1_RADIO_BUTTON)
+    twoButton = @builder.get_object(PLAYERS2_RADIO_BUTTON)
 
     oneButton.group =  zeroButton
     twoButton.group = zeroButton
 
     player2Name = @builder.get_object('player2_entry_box')
     player2Name.set_sensitive FALSE
+
+    player1Name = @builder.get_object('player1_entry_box')
+    player1Name.set_sensitive FALSE
+
+    @player1 = :computer
+    @player2 = :human
+    zeroButton.group.each do |item|
+      item.signal_connect('toggled') do |button|
+        #todo could switch here
+        if button.active?
+          if button == zeroButton
+            @player1 = :computer
+            @player2 = :computer
+            player2Name.set_sensitive FALSE
+            player1Name.set_sensitive FALSE
+          elsif button == oneButton
+            @player1 = :human
+            @player2 = :computer
+            player1Name.set_sensitive TRUE
+            player2Name.set_sensitive FALSE
+          elsif button == twoButton
+            @player1 = :human
+            @player2 = :computer
+            player1Name.set_sensitive TRUE
+            player2Name.set_sensitive TRUE
+          end
+        end
+      end
+    end
 
   end
 
