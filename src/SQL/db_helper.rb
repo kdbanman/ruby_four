6944859318701386c3da2f48e0e7467b73d13641
ryant26 <ryant26@gmail.com>
2015-03-30 19:55:53 -0600

    DB Helper contracts finished Changed Game Stats Table

--- src/SQL/db_helper.rb
+++ src/SQL/db_helper.rb
@@ -7,6 +7,41 @@ class DbHelper
 
   end
 
+  # @param [Integer] player_id
+  def add_win(player_id)
+    #pre
+    CommonContracts.integers id
+  end
+
+  # @param [Integer] player_id
+  def add_loss(player_id)
+    #pre
+    CommonContracts.integers id
+  end
+
+  # @param [Integer] player_id
+  def add_draw(player_id)
+    #pre
+    CommonContracts.integers id
+  end
+
+  # @param [String] data
+  # @param [Integer] player1
+  # @param [Integer] player2
+  def add_saved_game(data, player1, player2)
+    #pre
+    CommonContracts.integers player1, player2
+    CommonContracts.strings data
+  end
+
+  # @param [String] username
+  # @param [String] password
+  # @return [Integer] playerID
+  def add_user(username, password)
+    #pre
+    CommonContracts.strings username, password
+  end
+
   # @param [String] username
   # @param [String] password
   # @return [Integer] userID
@@ -17,6 +52,13 @@ class DbHelper
 
   # @param [Integer] id
   def get_saved_game(id)
+    #pre
+    CommonContracts.integers id
+  end
+
+  # @param [Integer] id
+  # @return [Array] all stats
+  def get_player_stats(id)
     #pre
     CommonContracts.integers id
   end
@@ -38,10 +80,10 @@ end
 
 module DBConstants
 
-  Users = 'Users'
-  GameStats = 'GameStats'
-  SavedGames = 'SavedGames'
-  Username = 'admin'
-  Password = 'admin'
+  USERS = 'Users'
+  GAME_STATS = 'GameStats'
+  SAVED_GAMES = 'SavedGames'
+  USERNAME = 'admin'
+  PASSWORD = 'admin'
 
 end
--- src/SQL/rebuild_db_schema.rb
+++ src/SQL/rebuild_db_schema.rb
@@ -6,30 +6,32 @@ conn = Mysql.new('127.0.0.1', 'ece421usr2', 'a421Psn101', 'ece421grp2', 13020)
 begin
   puts 'Building Datbase'
 
-  conn.query "DROP TABLE IF EXISTS #{DBConstants::Users}"
+  conn.query "DROP TABLE IF EXISTS #{DBConstants::USERS}"
 
-  conn.query "CREATE TABLE #{DBConstants::Users}(Id INT PRIMARY KEY AUTO_INCREMENT," +
+  conn.query "CREATE TABLE #{DBConstants::USERS}(Id INT PRIMARY KEY AUTO_INCREMENT," +
        ' Username VARCHAR(50) UNIQUE,' +
        ' Password VARCHAR(50))'
 
-  conn.query "DROP TABLE IF EXISTS #{DBConstants::GameStats}"
-  conn.query "CREATE TABLE #{DBConstants::GameStats}(Id INT," +
+  conn.query "DROP TABLE IF EXISTS #{DBConstants::GAME_STATS}"
+  conn.query "CREATE TABLE #{DBConstants::GAME_STATS}(Id INT," +
       ' Wins INT,' +
       ' Losses INT,' +
       ' Draws INT,' +
       ' PRIMARY KEY (Id),' +
-      " FOREIGN KEY (Id) REFERENCES #{DBConstants::Users}(Id))"
+      " FOREIGN KEY (Id) REFERENCES #{DBConstants::USERS}(Id))"
 
-  conn.query "DROP TABLE IF EXISTS #{DBConstants::SavedGames}"
-  conn.query "CREATE TABLE #{DBConstants::SavedGames}(Id INT PRIMARY KEY AUTO_INCREMENT," +
-      ' data BLOB)'
+  conn.query "DROP TABLE IF EXISTS #{DBConstants::SAVED_GAMES}"
+  conn.query "CREATE TABLE #{DBConstants::SAVED_GAMES}(Id INT PRIMARY KEY AUTO_INCREMENT, Player1 INT, Player2 INT," +
+      ' data BLOB,' +
+       " FOREIGN KEY (Player1) REFERENCES #{DBConstants::USERS}(Id)," +
+       " FOREIGN KEY (Player2) REFERENCES #{DBConstants::USERS}(Id))"
 
-  conn.query "INSERT INTO #{DBConstants::Users}(Username, Password)" +
-      " VALUES ('#{DBConstants::Username}', '#{DBConstants::Password}')"
+  conn.query "INSERT INTO #{DBConstants::USERS}(Username, Password)" +
+      " VALUES ('#{DBConstants::USERNAME}', '#{DBConstants::PASSWORD}')"
 
    puts 'Data Base built. 1 User:'
-   puts "username: #{DBConstants::Username}"
-   puts "password: #{DBConstants::Password}"
+   puts "username: #{DBConstants::USERNAME}"
+   puts "password: #{DBConstants::PASSWORD}"
 rescue Mysql::Error => e
   puts e
 ensure
