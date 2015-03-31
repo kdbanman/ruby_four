c1c9b70ed0d1a86a869406c8716bfc562375cca0
ryant26 <ryant26@gmail.com>
2015-03-30 21:56:37 -0600

    Contracts for UI finished - Added some container classes for data passing - Added some listviews for the UI

---
+++ src/model/containers.rb
@@ -0,0 +1,27 @@
+require_relative '../util/common_contracts'
+
+class SavedGame
+  attr_reader :player_1_name, :player_2_name, :game_id
+
+  def initialize(player1, player2, game_id)
+    CommonContracts.strings player1, player2
+    CommonContracts.integers game_id
+    @player_1_name = player1
+    @player_2_name = player2
+    @game_id = game_id
+  end
+
+end
+
+class GameStat
+  attr_reader :player_name, :wins, :draws, :losses
+
+  def initialize(player, wins, draws, losses)
+    CommonContracts.strings player
+    CommonContracts.integers wins, draws, losses
+    @player_name = player
+    @wins = wins
+    @draws = draws
+    @losses = losses
+  end
+end
