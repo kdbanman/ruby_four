df032a14a62b182c1a04a4c1852e9afdb95b18e4
ryant26 <ryant26@gmail.com>
2015-04-07 15:33:30 -0600

    changed saved game to support multiple game types

--- src/model/containers.rb
+++ src/model/containers.rb
@@ -1,20 +1,22 @@
 require_relative '../util/common_contracts'
 
 class SavedGame
-  attr_reader :player_1_name, :player_2_name, :game_id
+  attr_reader :player_1_name, :player_2_name, :game_id, :game_type
 
-  def initialize(player1, player2, game_id)
+  def initialize(player1, player2, game_id, game_type)
     CommonContracts.strings player1, player2
     CommonContracts.integers game_id
+  CommonContracts.verify_type game_type
     @player_1_name = player1
     @player_2_name = player2
     @game_id = game_id
+    @game_type = game_type
   end
 
 end
 
 class GameStat
-  attr_reader :game_type :player_name, :wins, :draws, :losses
+  attr_reader :game_type, :player_name, :wins, :draws, :losses
 
   def initialize(game_type, player, wins, draws, losses)
     CommonContracts.strings player
