9b1ad3b7b2d1f7b5d07b617f9617fba3cfd1f4e5
kdbanman <kirby.banman@gmail.com>
2015-04-10 15:39:46 -0600

    implement existing stubs.  write contracts.  (for rpcgameserver

--- src/rpc_game_server.rb
+++ src/rpc_game_server.rb
@@ -19,21 +19,54 @@ class RPCGameServer
 
   @game_id
 
+  @clients
+  @exit_listeners
+
   public
+
+  def initialize
+    @clients = Array.new
+    @exit_listeners = Array.new
+
+    verify_invariants
+  end
 
   # for master server to call.  not intended for clients to call.
   # @param [GameConfig] config
-  # @param [Integer] game_id
+  # @param [String] game_id a uuid
   # @return [Board] the constructed board ready for moves
   def start_from_config(config, game_id)
     # preconditions
     is_true @config.nil?, 'start_from_config must only be called once'
-    is_int game_id
+    is_gameid game_id
 
     puts "Starting from config:\n#{config}"
     @config = config
     @game_type = GameTypeFactory.get_game_type(config)
     @board = Board.new(config, @game_type, game_id)
+
+    @game_id = game_id
+
+    verify_invariants
+
+    @board
+  end
+
+  # for master server to call.  not intended for clients to call.
+  # @param [Board] saved_game
+  def start_from_existing(saved_game)
+    # preconditions
+    is_true @config.nil?, 'start_from_config must only be called once'
+    is_board saved_game
+
+    puts "Starting from board:\n#{saved_game}"
+    @config = saved_game.config
+    @game_type = GameTypeFactory.get_game_type(@config)
+    @board = saved_game
+
+    @game_id = saved_game.game_id
+
+    verify_invariants
 
     @board
   end
@@ -46,32 +79,72 @@ class RPCGameServer
     check_winner if PlaceTokenCommand.new(player_id, column, token_type, @game_type).run(@board)
     #TODO check_playable (no winner possible)
 
+    # TODO wrap exception XMLRPC::FaultException
     # send board to registered clients
+    @clients.each { |client| client.call("#{@game_id}.remote_update_board", @board)}
 
     # postconditions
     token_delta_one @board
+
+    verify_invariants
     
     @board
   end
 
   # @param [Integer] playerID
   def exit_game(playerID)
-    # TODO save game, broadcast exit to registered clients
+
+    # TODO save game
+    # master server will save game with listener callback
+    @exit_listeners.each { |listener| listener.call(playerID) }
+
+    # TODO wrap exception XMLRPC::FaultException
+    @clients.each { |client| client.call("#{@game_id}.remote_exit_game", playerID)}
+
+    verify_invariants
   end
 
   def check_winner
     puts @board.most_recent_token
 
-    if (winner = @game_type.get_winner(@board))
+    winner = @game_type.get_winner(@board)
+    if winner
       puts "Player #{winner} wins"
       @board.set_winner(winner)
     end
+
+    verify_invariants
   end
 
   def register_client(ip, port)
+    #preconditions
+    is_ip ip
+    CommonContracts.positive_integers port
 
     # start new rpc client at ip, port
+    # methods will be called with '<game id>.<method>'
+    begin
+      client = XMLRPC::Client.new(ip, '/', port)
+      @clients.push(client)
+    rescue Errno::ECONNREFUSED => e
+      warn "Could not connect to client at #{ip}:#{port}"
+    end
 
+    verify_invariants
+  end
+
+  def register_exit_listener(&callback)
+    @exit_listeners.push(callback)
+
+    verify_invariants
+  end
+
+  private
+
+  def verify_invariants
+    is_true @clients.length >= 0 && @clients.length <= 2
+
+    @exit_listeners.all? { |listener| CommonContracts.block_callable listener }
   end
 
 end
--- src/rpc_game_server_contracts.rb
+++ src/rpc_game_server_contracts.rb
@@ -9,8 +9,18 @@ module  RPCGameServerContracts
     failure msg unless bool
   end
 
-  def is_int(*args)
-    CommonContracts.integers *args
+  def is_gameid(*args)
+    CommonContracts.is_gameid *args
+  end
+
+  def is_board(board)
+    failure 'must be passed a Board' unless board.is_a? Board
+  end
+
+  def is_ip(ip)
+    unless ip.is_a?(String) && ip =~ /\d+\.\d+\.\d+\.\d+/
+      failure "#{ip} is not an ip"
+    end
   end
 
   # @param [Board] board
