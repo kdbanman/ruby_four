5401eb6f66aa43d40065c4ed34f3b33e27957899
ryant26 <ryant26@gmail.com>
2015-04-10 17:29:43 -0600

    SQL Helper all code finished, one test

--- src/SQL/db_helper.rb
+++ src/SQL/db_helper.rb
@@ -14,7 +14,7 @@ class DbHelper
     CommonContracts.integers player_id
     CommonContracts.verify_type game_type
 
-    #TODO game type!
+    runQuery build_game_stats_query(player_id, 'Wins', game_type)
   end
 
   # @param [Integer] player_id
@@ -24,7 +24,7 @@ class DbHelper
     CommonContracts.integers player_id
     CommonContracts.verify_type game_type
 
-    #TODO game type!
+    runQuery build_game_stats_query(player_id, 'Losses', game_type)
   end
 
   # @param [Integer] player_id
@@ -34,16 +34,18 @@ class DbHelper
     CommonContracts.integers player_id
     CommonContracts.verify_type game_type
 
-    #TODO game type!
+    runQuery build_game_stats_query(player_id, 'Draws', game_type)
   end
 
   # @param [String] data
   # @param [Integer] player1
   # @param [Integer] player2
-  def add_saved_game(data, player1, player2)
+  # @param [String] id
+  def add_saved_game(id, data, player1, player2)
     #pre
     CommonContracts.integers player1, player2
     CommonContracts.strings data
+    runQuery "INSERT INTO #{DBConstants::SAVED_GAMES}(Game_id, Player1, Player2, data) VALUES('#{id}', #{player1}, #{player2}, '#{data}')"
   end
 
   # @param [String] username
@@ -52,6 +54,8 @@ class DbHelper
   def add_user(username, password)
     #pre
     CommonContracts.strings username, password
+    runQuery "INSERT INTO #{DBConstants::USERS}(Username, Password) VALUES('#{username}', '#{password}')"
+    get_user_id(username, password)
   end
 
   # @param [String] username
@@ -60,6 +64,7 @@ class DbHelper
   def get_user_id(username, password)
     #pre
     CommonContracts.strings username, password
+    runQuery("SELECT Id FROM #{DBConstants::USERS} WHERE Username = '#{username}' AND Password = '#{password}'").each &(Proc.new {|row| return row['Id'].to_i})
   end
 
   # @param [Integer] id
@@ -67,6 +72,7 @@ class DbHelper
   def get_user_name(id)
     #pre
     CommonContracts.integers id
+    runQuery("SELECT Username FROM #{DBConstants::USERS} WHERE Id = #{id}").each_hash Proc.new {|res| return res['Username']}
   end
 
   # @param [String] username
@@ -74,12 +80,16 @@ class DbHelper
   def user_exists(username)
     #pre
     CommonContracts.strings username
+    out = Proc.new {}
+    runQuery("SELECT * FROM #{DBConstants::USERS} WHERE Username = '#{username}'").each_hash &(Proc.new { |user| return true})
+    return false
   end
 
-  # @param [Integer] id
+  # @param [String] id
   def get_saved_game(id)
     #pre
-    CommonContracts.integers id
+    CommonContracts.strings id
+    runQuery("SELECT data FROM #{DBConstants::SAVED_GAMES} WHERE Game_id = '#{id}'").each_hash Proc.new {|row| return row['data']}
   end
 
   # @param [Integer] id
@@ -91,7 +101,7 @@ class DbHelper
     CommonContracts.verify_type game_type
 
     #TODO game_type!
-
+    runQuery("SELECT Wins, Losses, Draws FROM #{DBConstants::GAME_STATS} WHERE Game_type = '#{game_type.to_s} AND Id = #{id}'").each_hash Proc.new {|row| return Array.new(row['Wins'], row['Losses'], row['Draws'])}
     #return [<wins>, <lossses>, <draws>]
   end
 
@@ -110,25 +120,32 @@ class DbHelper
     #      stats.add_stat('bill',  :otto,     0, 17, 1)
     #      stats.add_stat('bill',  :connect4, 1, 93, 3)  # bill sucks
     #      stats.add_stat('steve', :otto,     9,  0, 0)
+    users = {}
+    runQuery("SELECT Id, Username FROM #{DBConstants::USERS}").each_hash { |row| users[row['Id']] = row['Username']}
 
+    stats = GameStats.new
+    runQuery("SELECT * FROM #{DBConstants::GAME_STATS}").each_hash { |row| stats.add_stat_row(users[row['Id']], row['Game_type'], row['Wins'], row['Losses'], row['Draws'])}
+    return stats
   end
 
-  # @param [Integer] id
+  # @param [String] id
   def update_saved_game(id, data)
     #pre
-    CommonContracts.integers id
+    CommonContracts.strings id
+    runQuery "UPDATE #{DBConstants::SAVED_GAMES} SET data='#{data}' WHERE Game_id = '#{id}'"
   end
 
-  # @param [Integer] id
+  # @param [String] id
   def delete_saved_game(id)
     #pre
-    CommonContracts.integers id
+    CommonContracts.strings id
+    runQuery "DELETE FROM #{DBConstants::SAVED_GAMES} WHERE Game_id = '#{id}'"
   end
 
   private
   def get_connection
     begin
-      @conn = Mysql.new('mysqlsrv.ece.ualberta.ca', 'ece421usr2', 'a421Psn101', 'ece421grp2', '13020')
+      @conn = Mysql.new('mysqlsrv.ece.ualberta.ca', 'ece421usr2', 'a421Psn101', 'ece421grp2', 13020)
     rescue Mysql::Error
       raise ContractFailure 'Could not connect to DataBase'
     end
@@ -136,6 +153,23 @@ class DbHelper
 
   def close_connection
     @conn.close if @conn
+  end
+
+  def runQuery(query)
+    begin
+      get_connection
+      return @conn.query query
+    rescue Mysql::Error => e
+      puts "ERROR: Query *#{query}* did not complete for reason: #{e.error}"
+      raise "Query *#{query}* did not complete for reason: #{e.error}"
+    ensure
+      close_connection
+    end
+  end
+
+  def build_game_stats_query(id, field, game_type)
+    "INSERT INTO #{DBConstants::SAVED_GAMES}(Id, Game_type, #{field}) VALUES(#{id}, 1, '#{game_type.to_s}') ON DUPLICATE KEY " +
+        "UPDATE #{field}=#{field} + 1"
   end
 
 end
