aa9237454d1aa86461afb9e23e93031d0e10bd4c
ryant26 <ryant26@gmail.com>
2015-04-10 20:23:36 -0600

    Stats menu item hooked up

--- src/view/main_screen.rb
+++ src/view/main_screen.rb
@@ -62,9 +62,10 @@ class MainScreen
 
     @builder.get_object('new_game_menuitem').signal_connect('activate') {@new_game_listener.call}
     @builder.get_object('load_game_menuitem').signal_connect('activate') {@load_game_listener.call}
-    @builder.get_object('stats_menuitem').signal_connect('activate') {@stats_listener}
+    @builder.get_object('stats_menuitem').signal_connect('activate') {@stats_listener.call}
     @builder.get_object('quit_menuitem').signal_connect('activate') {kill}
     @builder.get_object('about_menuitem').signal_connect('activate') {AboutScreen.new}
+
 
     renderer = Gtk::CellRendererText.new
 
